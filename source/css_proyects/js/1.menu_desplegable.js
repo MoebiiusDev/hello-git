9bf19b445f320392b7e50808e0c72b2ce7f9c104
MoebiiusDev <marianocornejo.vpx@gmail.com>
2024-08-10 03:22:19 -0400

    Se modifico el html de menu-desplegable, actualizar el nav de las paginas principales, git, index, css_page, tercer easter egg, diria que falta un detalle pero la atmosfera es impresionante

--- source/css_proyects/js/1.menu_desplegable.js
+++ source/css_proyects/js/1.menu_desplegable.js
@@ -1,9 +1,6 @@
 const bMore = document.querySelector('#bMore')
 const links = document.querySelector('#links')
 
-
-
-
 bMore.addEventListener('click', () => {
     links.classList.toggle('collapse')
 })
@@ -35,3 +32,42 @@ epa.forEach((elemento) => {
         }
     });
 });
+
+const lamentoso = document.querySelector("#lamentoso");
+const logo = document.querySelectorAll(".logo"); // Reemplaza con el ID de tu elemento
+const nevada_effect = document.querySelector(".nevada_effect")
+let lamentosoPlay = false;
+let timeoutId;
+let currentVolume = 0.1; // Volumen inicial (puede ser ajustado según tus necesidades)
+const maxVolume = 1; // Volumen máximo
+
+logo.forEach((elemento2) => {
+    elemento2.addEventListener('mouseover', () => {
+        timeoutId = setTimeout(() => {
+            document.body.style.filter = "grayscale(100%)";
+            document.body.style.transition = "all 2s";
+
+
+            if (!lamentosoPlay) {
+                lamentosoPlay = true;
+            }
+            if (currentVolume < maxVolume) {
+                currentVolume += 0.3;
+                lamentoso.volume = currentVolume;
+                lamentoso.play();
+            }
+        }, 5000);
+        setTimeout(function nevadaF() {
+            nevada_effect.style.display = 'flex';
+
+        }, 20000);
+    });
+    elemento2.addEventListener("mouseout", () => {
+        // Cancela el temporizador si el mouse sale del elemento antes de los 2 segundos
+        clearTimeout(timeoutId);
+        currentVolume = 0.2;
+        lamentoso.volume = currentVolume;
+    })});
+
+
+
